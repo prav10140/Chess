ae6601f1b7cd5f53d6bb39b49560b710b9760af1
PRAVEEN PATEL <prav9406@gmail.com>
2025-01-17 15:41:48 +0530

    Update Square.js

--- Chess-game/src/components/Square.js
+++ Chess-game/src/components/Square.js
@@ -1,1 +1,33 @@
+import React from "react";
+
+function Square({ piece, isValidMove, onClick }) {
+    const getPieceSymbol = (piece) => {
+        const symbols = {
+            wK: "♔",
+            wQ: "♕",
+            wR: "♖",
+            wB: "♗",
+            wN: "♘",
+            wP: "♙",
+            bK: "♚",
+            bQ: "♛",
+            bR: "♜",
+            bB: "♝",
+            bN: "♞",
+            bP: "♟",
+        };
+        return symbols[piece] || "";
+    };
+
+    return (
+        <div
+            className={`square ${isValidMove ? "valid-move" : ""}`}
+            onClick={onClick}
+        >
+            {getPieceSymbol(piece)}
+        </div>
+    );
+}
+
+export default Square;
 
