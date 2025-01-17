5141b6193fb1b61e39bf6a570a374de8c0631e89
PRAVEEN PATEL <prav9406@gmail.com>
2025-01-17 15:41:01 +0530

    Update App.js

--- Chess-game/src/App.js
+++ Chess-game/src/App.js
@@ -1,1 +1,15 @@
+import React from "react";
+import Chessboard from "./components/Chessboard";
+import "./styles/styles.css";
+
+function App() {
+    return (
+        <div className="App">
+            <h1>Online Chess Game</h1>
+            <Chessboard />
+        </div>
+    );
+}
+
+export default App;
 
